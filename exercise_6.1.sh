002dd4c35681049ea8809fd5eab2f5ec3b15a991
SilverHairtail <114496700+SilverHairtail@users.noreply.github.com>
2022-10-06 13:27:15 -0400

    Delete exercise_6.1.sh

--- exercise_6.1.sh
+++
@@ -1,5 +0,0 @@
-#usage: bash excersice_6.1.sh wages.csv
-for gender in female male
-do
-    cat $1 | cut -d ',' -f 1,2 | grep -e "^$gender" | sort -t ',' -k 2 -n | uniq >> unique_combination.txt
-done
